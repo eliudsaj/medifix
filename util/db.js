b615629c0a4dabeb0bb3649eceb5885a0f68ccea
eliudsaj <eliudkaranja5@gmail.com>
2025-06-09 10:52:12 +0300

    edit for db.

--- util/db.js
+++ util/db.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 
 // Get values from environment variables
-const DB_NAME = process.env.DB_NAME || 'cmms';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'mysql';
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_NAME = process.env.DB_NAME || 'cmms_1ee4';
+const DB_USER = process.env.DB_USER || 'cmms_1ee4_user';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'oj9F2C6vwEfASBnYuMUbKCcmGfLPxCvG';
+const DB_HOST = process.env.DB_HOST || 'dpg-d1382rc9c44c7390kh6g-a';
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
