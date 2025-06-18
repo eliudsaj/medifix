051ea50130374da87cf5ade12ff841915387d062
eliudsaj <eliudkaranja5@gmail.com>
2025-06-18 16:17:29 +0300

    edit db

--- util/db.js
+++ util/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const DB_NAME = process.env.DB_NAME || 'sql8783807';
-const DB_USER = process.env.DB_USER || 'sql8783807';
-const DB_PASSWORD = process.env.DB_PASSWORD || '4cwJkscemh';
-const DB_HOST = process.env.DB_HOST || 'sql8.freesqldatabase.com';
+const DB_NAME = process.env.DB_NAME || 'sql12785519';
+const DB_USER = process.env.DB_USER || 'sql12785519';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'LXuJND7J3t';
+const DB_HOST = process.env.DB_HOST || 'sql12.freesqldatabase.com';
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
